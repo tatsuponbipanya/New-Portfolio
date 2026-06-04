381d7f6afa85136764a024d11048085702da8ed3
tatsuponbipanya <tatsuponbipanya@gmail.com>
2026-06-04 16:42:49 +0900

    createメソッドを追加　入力データ保存可能

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,34 @@
 class HomeController < ApplicationController
+
   def index
+    # 新しく記録するための空のインスタンス作成（入力フォームの準備に必要）
+    @workout_log = WorkoutLog.new
+    # 過去の記録を表示する
+    @workout_logs = WorkoutLog.order(workout_date: :desc)
+  end
+
+  def create
+    # 入力されたデータを入れてインスタンス作成
+    @workout_log = WorkoutLog.new(workout_log_params)
+    #workout_log_paramsは、privateで設定したセキュリティチェックの済んだデータ。
+
+    if @workout_log.save
+      redirect_to root_path, notice: "筋肉が記録されました。"
+    else
+      @workout_logs = WorkoutLog.order(workout_date: :desc)
+      render :index, status: :unprocessable_entity
+      #status: :unprocessable_entityで422 (リクエストは届いたけど、データがダメで処理できない）を明治しないと、
+      #Hotwire（Turbo）が正常にエラー画面をレンダリングしてくれずバグの原因になる
+    end
+  end
+
+  private
+  #カプセル化（隠蔽）。外からURL（インターネット）経由で呼び出せないメソッド。
+  #ハッカーがURLの入力を工夫して、以下のメソッドを直接狙い撃ちしてきても無効に出来る。
+
+  def workout_log_params
+    params.require(:workout_log).permit(:workout_date, :menu_type, :weight, :reps)
+    #セキュリティ。ちゃんとworkout_logのデータがあるかのチェックと、日付、メニュー名、重量、回数の4つのデータだけを通過許可。
+    #これがないと、admin: true（自分を管理者権限にする）などのデータが通ってしまう。
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,9 @@ Rails.application.routes.draw do
   # root "posts#index"
 
   root "home#index"
-  resources :workout_logs
+  post "workout_logs", to: "home#create", as: :workout_logs
+  #asは、ビュー（HTML）側で workout_logs_path っていう**便利な近道用の名前（あだ名）**を使えるようにする設定。
+  #複数のデータを扱う場合、必ず複数形（s）で設定する。でないとエラー。
+  #このあだ名がないと、ビューのform_withの1行目を、「URLを直接手書き」して書かなきゃいけなくなる。
+  #将来、URLを変更したくなったときも、ルーティングのファイルのURL部分を1箇所書き換えるだけで済む。
 end
