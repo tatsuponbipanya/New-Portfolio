898d3a8cd67cc1d5ddbfdf9a3423b0d7ee80e682
tatsuponbipanya <tatsuponbipanya@gmail.com>
2026-06-05 15:23:02 +0900

    種目の入力フォームをプルダウン化

--- app/models/workout_log.rb
+++ app/models/workout_log.rb
@@ -1,7 +1,7 @@
 class WorkoutLog < ApplicationRecord
     validates :menu_type, presence: true
-    #数字で（ニューメリキャリティ）、0より大きい場合のみ許可
-    validates :weight, numericality: { greater_than: 0 }
+    #数字で（ニューメリキャリティ）、0以上の場合のみ許可
+    validates :weight, numericality: { greater_than_or_equal_to: 0 }
     #数字で（ニューメリキャリティ）、整数（integer）の、0より大きい場合のみ許可
     validates :reps, numericality: { only_integer: true, greater_than: 0 }
 end
