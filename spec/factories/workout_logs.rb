5f3ba3c665c698913bcfb63bb18dab650d4fcdc4
tatsuponbipanya <tatsuponbipanya@gmail.com>
2026-06-30 18:04:03 +0900

    rubocop修正

--- spec/factories/workout_logs.rb
+++ spec/factories/workout_logs.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :workout_log do
-      # ユーザーと紐づける
-      association :user
+    # ユーザーと紐づける
+    association :user
 
-      workout_date { Date.today }
-      menu_type { 'ベンチプレス' }
-      weight { 83.0 }
-      reps { 1 }
+    workout_date { Date.today }
+    menu_type { 'ベンチプレス' }
+    weight { 83.0 }
+    reps { 1 }
   end
 end
